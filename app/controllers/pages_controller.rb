13873c053932af2a43a7b8d9700204ac5e087a1a
azsro <highflight109@msn.com>
2015-08-25 23:45:54 +0000

    Second Commit. Added Home,Login,Explore.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def Home
+  end
+
+  def Login
+  end
+
+  def Explore
+  end
+
+  def About
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'pages#Home'
+  
+  # Define routes for Pages
+  get 'pages/Home' => 'pages#Home'
 
+  get 'pages/Login' => 'pages#Login'
+
+  get 'pages/Explore' => 'pages#Explore'
+
+  get 'pages/About' => 'pages#About'
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
