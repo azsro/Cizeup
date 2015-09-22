1933c2a7684bae6f056693cc03b9b571d81af8a0
azsro <highflight109@msn.com>
2015-09-22 20:48:16 +0000

    22nd commit. Log in/out works.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Cizeup!"
+      flash[:success] = "Welcome to Cizeup!"
       redirect_to @user
     else
       render 'new'
