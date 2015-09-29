7c6030f0783d6227558145009275a427a9c618b2
azsro <highflight109@msn.com>
2015-09-29 19:10:53 +0000

    28th commit. added password reset.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -54,6 +55,12 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   
   delete 'logout'  => 'sessions#destroy'
+  
+  # Other Routing 
+  
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
