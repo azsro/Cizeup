f8e2a0796b687978d1389b23c475159bcc0d2cc2
azsro <highflight109@msn.com>
2015-09-30 20:19:42 +0000

    30th commit. Added posts. errors not working.

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,27 @@
+class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  
+  def new
+    @micropost = current_user.microposts.build if logged_in?
+  end
+  
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Post created!"
+      redirect_to current_user
+    else
+      flash[:warning] = "An error occured."
+      redirect_to(:back)
+    end
+  end
+
+  def destroy
+  end
+  
+    private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+    @micropost = current_user.microposts.build if logged_in?
   end
   
   def create
@@ -54,15 +56,6 @@ class UsersController < ApplicationController
     
     # Before filters
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-       store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-    
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
