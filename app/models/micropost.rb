206b2b2c9e750c32418e8c4a554bd98b7b3ea6d9
azsro <highflight109@msn.com>
2015-10-07 22:55:26 +0000

    34th commit. Added following.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,5 @@ class Micropost < ActiveRecord::Base
  belongs_to :user
  default_scope -> { order(created_at: :desc) }
  validates :user_id, presence: true
- validates :content, presence: true, length: { maximum: 200 }  # 270000 Max length approx equal to 100 pages of writing, including spaces, for now. 
+ validates :content, presence: true, length: { maximum: 270000 }  # 270000 Max length approx equal to 100 pages of writing, including spaces, for now. 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -64,6 +71,8 @@ Rails.application.routes.draw do
   get 'password_resets/new'
 
   get 'password_resets/edit'
+  
+  
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
